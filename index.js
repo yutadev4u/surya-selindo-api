ec2e275830218219d0931aafae2f3e213c92cec9
yutadev4u <zoommtommy@gmail.com>
2024-09-04 13:31:41 +0700

    a

--- index.js
+++ index.js
@@ -33,3 +33,6 @@ const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
+
+module.exports = app
+module.exports.handler = serverless(app)
